5d0c3d4d2ba0958dde8d66e9eb2cfca8a73e8e02
edgarmejiav <edgarm_10@hotmail.com>
2022-07-19 21:39:57 -0500

    final app - lega

--- themes/dark-theme.ts
+++ themes/dark-theme.ts
@@ -1,29 +1,27 @@
-import { createTheme } from '@mui/material';
-import { red } from '@mui/material/colors';
-
-
+import { red } from "@mui/material/colors";
+import { createTheme } from "@mui/material/styles";
 
 export const darkTheme = createTheme({
-    palette: {
-        mode: 'dark',
-        secondary: {
-            main: '#19857b'
-        },
-        error: {
-            main: red.A400
-        },
-        },
+  palette: {
+    mode: "dark",
+    secondary: {
+      main: "#19857b",
+    },
+    error: {
+      main: red.A400,
+    },
+  },
 
-        components: {
-            MuiAppBar: {
-                defaultProps: {
-                    elevation: 0
-                },
-                styleOverrides: {
-                    root: {
-                        backgroundColor: '#4a148c'
-                    }
-                }
-            }
-        }
+  components: {
+    MuiAppBar: {
+      defaultProps: {
+        elevation: 0,
+      },
+      styleOverrides: {
+        root: {
+          backgroundColor: "#4a148c",
+        },
+      },
+    },
+  },
 });
